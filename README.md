b3e413b02b583c95b738252e85e7b369bce29d7e
lafeillou <lafeillou@126.com>
2023-03-10 15:45:51 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-rdpuyq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-rdpuyq)
---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref, nextTick } from 'vue';
+const count = ref(0);
+
+async function increment() {
+  count.value++;
+
+  console.log(document.getElementById('counter').textContent);
+
+  await nextTick();
+
+  console.log(document.getElementById('counter').textContent);
+}
+</script>
+
+<template>
+  <div>
+    <button id="counter" @click="increment">{{ count }}</button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+body {
+  button {
+    font-size: 20px;
+  }
+}
+</style>
